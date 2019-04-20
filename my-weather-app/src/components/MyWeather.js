3dc490437492267d41eb2f7c99f1afaf793d93fa
Madhavi Palteru <madhavi.palteru@gmail.com>
2019-04-20 21:40:20 +0100

    adding css

--- my-weather-app/src/components/MyWeather.js
+++ my-weather-app/src/components/MyWeather.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import SunnyIcon from "../css/images/sunnyIcon.png"
+//import "../css/OpenWeather.css";
 import OpenWeather from "../components/OpenWeatherApp";
 //import CloudyIcon from "../css/images/cloudyIcon.png"
 
@@ -31,13 +31,13 @@ class WeatherForecast extends Component {
     render() {
         return (
             <div>
-                <h1>Enter city name</h1>
-                <div className="container">
-                    <div className="col-sm-6">
-                        <input type="text" placeholder="Your city name" onChange={this.changeText.bind(this)} value={this.state.searchValue} />
-                        <button type="button" className="btn btn-primary" onClick={this.sendValue}>Search</button>
-                    </div>
+<form className="form-inline">
+                <div className="form-group mx-sm-3 mb-2">
+                    <input type="text" placeholder="Your city name" onChange={this.changeText.bind(this)} value={this.state.searchValue} className="form-control"/>
+                    
                 </div>
+                <button type="button" className="btn btn-primary mb-2" onClick={this.sendValue}>Search</button>
+                </form>
                 {this.state.sendValueToOpenWeather ? <OpenWeather cityName={this.state.sendValueToOpenWeather} /> : null}
             </div>
 
--- my-weather-app/src/components/OpenWeatherApp.js
+++ my-weather-app/src/components/OpenWeatherApp.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import SunnyIcon from "../css/images/sunnyIcon.png"
-import CloudyIcon from "../css/images/cloudyIcon.png"
+
 
 export default class OpenWeather extends Component {
     constructor(props) {
@@ -27,8 +26,8 @@ export default class OpenWeather extends Component {
             <div>
                 {this.state.weatherData ? this.state.weatherData.map(
                     (value, index) =>
-                    <div>
-                        <p key={index}>{<img src={'http://openweathermap.org/img/w/' + value.icon + '.png'} alt="Sunny" />}</p>
+                    <div key={index}>
+                        <p>{<img src={'http://openweathermap.org/img/w/' + value.icon + '.png'} alt="Sunny" />}</p>
                         <label>{value.description}</label>
                         </div>
                 ) : this.state.error ? alert(this.state.error) : null}
