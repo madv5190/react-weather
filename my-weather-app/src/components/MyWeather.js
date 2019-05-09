035e330f810f0f36b651aa3648723bc299e9d590
Madhavi Palteru <madhavi.palteru@gmail.com>
2019-05-09 20:38:09 +0100

    Removed unnecessary state setup

--- my-weather-app/src/components/MyWeather.js
+++ my-weather-app/src/components/MyWeather.js
@@ -1,48 +1,48 @@
 import React, { Component } from "react";
-//import "../css/OpenWeather.css";
 import OpenWeather from "../components/OpenWeatherApp";
-//import CloudyIcon from "../css/images/cloudyIcon.png"
-
 
 class WeatherForecast extends Component {
-    constructor() {
-        super();
-        this.state = {
-            searchValue: "",
-            sendValueToOpenWeather: ""
-        }
-        this.changeText = this.changeText.bind(this);
-        this.sendValue = this.sendValue.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      searchValue: ""
+    };
 
-    changeText(event) {
-        this.setState({
-            searchValue: event.target.value
-        })
-    }
+    this.sendValue = this.sendValue.bind(this);
+  }
 
-    sendValue() {
-        if (this.state.searchValue)
-            this.setState({
-                sendValueToOpenWeather: this.state.searchValue
-            })
-
-    }
-    render() {
-        return (
-            <div>
-<form className="form-inline">
-                <div className="form-group mx-sm-3 mb-2">
-                    <input type="text" placeholder="Your city name" onChange={this.changeText.bind(this)} value={this.state.searchValue} className="form-control"/>
-                    
-                </div>
-                <button type="button" className="btn btn-primary mb-2" onClick={this.sendValue}>Search</button>
-                </form>
-                {this.state.sendValueToOpenWeather ? <OpenWeather cityName={this.state.sendValueToOpenWeather} /> : null}
-            </div>
-
-        );
-    }
-};
+  sendValue() {
+    if (this.input.value)
+      this.setState({
+        searchValue: this.input.value
+      });
+  }
+  render() {
+    return (
+      <div className="user">
+        <form className="form-inline">
+          <div className="form-group mx-sm-3 mb-2">
+            <input
+              type="text"
+              placeholder="Your city name"
+              ref={userInput => (this.input = userInput)}
+              className="form-control"
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary mb-2"
+            onClick={this.sendValue}
+          >
+            Search
+          </button>
+        </form>
+        {this.state.searchValue ? (
+          <OpenWeather cityName={this.state.searchValue} />
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default WeatherForecast;
--- my-weather-app/src/components/OpenWeatherApp.js
+++ my-weather-app/src/components/OpenWeatherApp.js
@@ -1,39 +1,59 @@
 import React, { Component } from "react";
 
-
 export default class OpenWeather extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            weatherData: [],
-            error: ""
-        };
-    }
-    componentDidMount() {
-        if (this.props.cityName) {
-            fetch('https://api.openweathermap.org/data/2.5/weather?q=' + this.props.cityName + '&appid=77ad1345ec7cafe86bb848ed03bb9a57')
-                .then(response => response.json())
-                .then(body => this.setState({ weatherData: body.weather })).catch(error => {
-                    this.setState({
-                        error: "Error fetching Weather Data from the Server"
-                    })
-                });
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: [],
+      error: ""
     };
+  }
+  componentDidMount() {
+    if (this.props.cityName) {
+      let url =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        this.props.cityName +
+        "&appid=xxxxxx";
 
-    render() {
-        return (
-            <div>
-                {this.state.weatherData ? this.state.weatherData.map(
-                    (value, index) =>
-                    <div key={index}>
-                        <p>{<img src={'http://openweathermap.org/img/w/' + value.icon + '.png'} alt="Sunny" />}</p>
-                        <label>{value.description}</label>
-                        </div>
-                ) : this.state.error ? alert(this.state.error) : null}
-
-            </div>
-        );
+      fetch(url)
+        .then(response => {
+          if (!response.ok)
+            throw new Error("Error fetching Weather Data from the Server");
+          else return response.json();
+        })
+        .then(body => this.setState({ weatherData: body.weather }))
+        .catch(error => {
+          this.setState({
+            error: error
+          });
+        });
     }
-}
+  }
 
+  render() {
+    return (
+      <div>
+        {this.state.weatherData
+          ? this.state.weatherData.map((value, index) => (
+              <div key={index}>
+                <p>The Current weather is:</p>
+                <p>
+                  {
+                    <img
+                      src={
+                        "http://openweathermap.org/img/w/" + value.icon + ".png"
+                      }
+                      alt="Sunny"
+                    />
+                  }
+                </p>
+                <label>{value.description}</label>
+              </div>
+            ))
+          : this.state.error
+          ? alert(this.state.error)
+          : null}
+      </div>
+    );
+  }
+}
